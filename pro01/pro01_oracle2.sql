031674950c6bc7f9e97f3977f2971c3d06a71da4
kkt09072 <ksb450424@naver.com>
2024-04-12 17:51:50 +0900

    pro01 qna module & traffic module

--- pro01/pro01_oracle2.sql
+++ pro01/pro01_oracle2.sql
@@ -51,11 +51,37 @@ desc traffic;
 
 alter table traffic modify route varchar2(1000);
 
+-- 교통편 등록
 insert into traffic values (tseq.nextval, '버스', '금천03', '가산디지털단지역 - 마리오아울렛 - 제일모직아울렛 - 구립가산도서관.두산위브아파트 - 가산동주민센터.금천글로벌센터 - 독산고개 - 구로디지털단지역', '서울특별시 금천구 마을버스로 대상운수에서 운행하는 마을버스 노선으로 왕복 운행거리는 7.2km이다. 1980년에 배치된 노선으로 현재까지 그 경로가 바뀐 적이 없는 귀한 노선이다.', 'https://map.naver.com/p/bus/bus-route/%EA%B8%88%EC%B2%9C03/bus-route/1528?c=15.00,0,0,0,dh');
 insert into traffic values (tseq.nextval, '버스', '21', '광명돔경륜장 - 광명6동삼거리 - 현진월드아파트 - 헵시바LPG충전소 - 광명사거리역.광명전통시장 - 광명시청.광명시민회관 - 광덕초등학교[단독] - 도덕파크타운[단독] - 광명경찰서 - 디지털3단지사거리 - 가산디지털단지역 - 디지털운동장 - 디지털3단지사거리', '경기도 광명시 시내버스로 화영운수에서 운행하는 시내버스 노선으로 왕복 운행거리는 15.4km이다.', 'https://map.naver.com/p/bus/bus-route/-/bus-route/12506?c=14.00,0,0,0,dh');
 
-select * from traffic;
+-- 교통편 검색
+select * from traffic order by ttype desc, no asc;
+
+-- 교통편 수정
+update traffic set route='', coment='' where tno=1;
+
+-- 교통편 삭제
+delete from traffic where tno=1;
 
 commit;
 
 select * from guide;
+
+-- 장소 검색
+select * from guide where pcode=1;
+
+-- 장소 등록
+insert into guide values (gseq.nextval, '','','','','','');
+insert into guide values (gseq.nextval, 'BYC하이시티','01','서울특별시 금천구 가산동 가산디지털1로 131','1544-7871','37.4769693,126.8824741','디지털로의 대로변에 위치한 빌딩입니다.');
+insert into guide values (gseq.nextval, 'G밸리산업협회','02','서울특별시 금천구 디지털로9길 32 B동 1406호(가산동, 갑을그레이트밸리)','02-867-2100','37.4794491,126.8875147','남부순환로의 금천경찰서 가산치안센터의 뒷 건물에 위치한 협회입니다.');
+insert into guide values (gseq.nextval, '중화요리 연','03','서울특별시 금천구 가산디지털1로 225, 에이스 가산 포휴 1층 116호 (가산동)','전화번호 없음','37.4851258,126.8787374','가산디지털1로의 에이스가산포휴 빌딩에 위치한 중국요리 맛집입니다.');
+insert into guide values (gseq.nextval, '더스테이 호텔','04','서울특별시 금천구 가산동 139-25','010-3137-1810','37.4792982,126.8886446','깨끗한 숙박시설');
+
+-- 장소 수정
+update guide set pname='', paddr='', ptel='', pgps='', pcoment='' where pcode=1;
+
+-- 장소 삭제
+delete from guide where pcode=1;
+
+-- ptype : 01-주요 빌딩, 02-주요 협회 단체, 03-맛집안내, 04-숙박시설, 05-교육시설, 06-공원 및 스포츠 시설, 07-상업시설, 08-기타시설 
